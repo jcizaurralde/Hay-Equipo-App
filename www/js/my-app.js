c139093be56fa7296114f2d7b503722199301f3f
jcizaurralde <jcizaurralde@gmail.com>
2021-03-12 08:23:42 -0300

    Vista registro usuarios administradores (complejos canchas de futbol) + Creacion de base de datos de usuarios administradores

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -19,13 +19,13 @@ var app = new Framework7({
       { path: '/index/', url: 'index.html', },
       { path: '/registro/', url: 'registro.html', },
       { path: '/mi-equipo/', url: 'mi-equipo.html', },
+      { path: '/registro-canchas/', url: 'registro-canchas.html', },
     
     ]
     // ... other parameters
   });
 
 var mainView = app.views.create('.view-main');
-
 var email = "";
 var password = "";
 var password2 = "";
@@ -36,7 +36,17 @@ var emailLogin="";
 var passLogin="";
 var db = "";
 var colUsuarios ="";
+var colUsuariosAdm = "";
 var nombreLogin = "";
+var emailAdmin = ""; 
+var passwordAdmin = ""; 
+var password2Admin = ""; 
+var nombreAdmin = ""; 
+var calleAdmin = ""; 
+var alturaAdmin = ""; 
+var localidadAdmin = ""; 
+var CPAdmin = ""; 
+var provinciaAdmin = "";
 
 //REVISAR ESTA NOTIFICACION
 /*var notificationWithButton = $$.notification.create({
@@ -53,6 +63,7 @@ $$(document).on('deviceready', function() {
     console.log("Device is ready!");
     db = firebase.firestore();
     colUsuarios = db.collection("Usuarios");
+    colUsuariosAdm = db.collection("Usuarios Administrativos");
 });
 
 // Option 1. Using one 'page:init' handler for all pages
@@ -69,6 +80,7 @@ $$(document).on('page:init', '.page[data-name="ingreso"]', function (e) {
     /*$$('#btnVolverMenu').on('click', fnCierraPanel);*/
     $$('#btnMiequipo').on('click', fnMiequipo);
     var usuRef = colUsuarios.doc(emailLogin);
+    //Tomo de la colección Usuarios de la BD, el dato "Nombre", para poder darle una bienvenida al usuario en el inicio.
     usuRef.get().then((doc) => {
       if (doc.exists) {
         console.log("Document data:", doc.data());
@@ -82,24 +94,22 @@ $$(document).on('page:init', '.page[data-name="ingreso"]', function (e) {
     }).catch((error) => {
       console.log("Error getting document:", error);
     });
-
-    //Esta funcion hay que corregirla, solo con singOut()
+    //Función para cerrar cesión.
     function fnLogout() {
-      var cierraCesion = () => {
-        var user = firebase.auth().currentUser;
-        if (user) {
-          firebase.auth().signOut()
-            .then(() => {
-              console.log('Cerrar sesión');
-              mainView.router.navigate('/index/');
-            })
-            .catch((error) => {
-              console.log('error ' + error);
-            });
-        } else {
-          console.log('Ya cerre sesion');
-        }
+      var user = firebase.auth().currentUser;
+      if (user) {
+        firebase.auth().signOut()
+          .then(() => {
+            console.log('Cerrar sesión');
+            mainView.router.navigate('/index/');
+          })
+          .catch((error) => {
+            console.log('error ' + error);
+          });
+      } else {
+        console.log('Ya cerre sesion');
       }
+      
     }
 
     //REVISAR EL CIERRE DEL PANEL
@@ -118,19 +128,65 @@ $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
   $$('#btnRegVolver').on('click', function fnVuelveIndex() {
     mainView.router.navigate('/index/');
   });
-  
+  //Funcion de registro de un nuevo usuario con correo electronico como ID.
   function fnRegistrar() {
     email = $$('#registroEmail').val();
     password = $$('#registroPassword').val();
-    /*password2 = $$('#registroPassword2').val();*/
+    password2 = $$('#registroPassword2').val();
     nombre = $$('#registroNombre').val();
     nacimiento = $$('#registroNacimiento').val();
     console.log("email: " + email);
     console.log("contraseña: " + password);
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+    if (password == password2){
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then((user) => {
+            var datos = {Nombre: nombre, FechaNacimiento: nacimiento };
+            colUsuarios.doc(email).set(datos);
+          })
+          .catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+              alert('Clave muy débil.');
+            } else {
+              alert(errorMessage);
+            }
+            console.log(error);
+          });
+        /*notificationWithButton.open();*/
+        mainView.router.navigate('/index/');
+    }else{
+    console.log("Las contraseñas no coinciden, vuelva a ingresar contraseña");
+    }
+  }
+})
+
+$$(document).on('page:init', '.page[data-name="registro-canchas"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log(e);
+  $$('#btnRegComp').on('click',fnRegAdmnistrador);
+  $$('#btnVolverComp').on('click', function fnVuelveIndexComp() {
+    mainView.router.navigate('/index/');
+  });
+//Funcion de registro de un nuevo usuario administrador (complejos de canchas) con correo electronico como ID.
+  function fnRegAdmnistrador() {
+    emailAdmin = $$('#regEmailAdmin').val();
+    passwordAdmin = $$('#regPassAdmin').val();
+    password2Admin = $$('#regPass2Admin').val();
+    nombreAdmin = $$('#regNombreAdmin').val();
+    calleAdmin = $$('#regCalleAdmin').val();
+    alturaAdmin = $$('#regAlturaAdmin').val();
+    localidadAdmin = $$('#regLocalidaddmin').val();
+    provinciaAdmin = $$('#regProvinciaaddmin').val();
+    CPAdmin = $$('#regCPAdmin').val();
+    console.log("email: " + emailAdmin);
+    console.log("contraseña: " + passwordAdmin);
+    if (passwordAdmin == password2Admin){
+      firebase.auth().createUserWithEmailAndPassword(emailAdmin, passwordAdmin)
         .then((user) => {
-          var datos = {Nombre: nombre, FechaNacimiento: nacimiento };
-          colUsuarios.doc(email).set(datos);
+          var datosAdm = { NombreComplejo: nombreAdmin, Calle: calleAdmin, Altura: alturaAdmin, 
+          Localidad: localidadAdmin, Provincia:provinciaAdmin, CodigoPostal: CPAdmin };
+          colUsuariosAdm.doc(emailAdmin).set(datosAdm);
         })
         .catch(function (error) {
           var errorCode = error.code;
@@ -144,7 +200,11 @@ $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
         });
       /*notificationWithButton.open();*/
       mainView.router.navigate('/index/');
-    } 
+    }else{
+    console.log("Las contraseñas no coinciden, vuelva a ingresar contraseña");
+    }
+  }
+  
 })
 
 $$(document).on('page:init', '.page[data-name="mi-equipo"]', function (e) {
@@ -173,6 +233,7 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
   console.log(e);
   $$('#btnIngresar').on('click', fnIngreso);
   $$('#btnRegistro').on('click', fnRegistro);
+  $$('#btnRegComplejo').on('click', fnRegComplejo);
 
   function fnIngreso() {
     $$('.login-screen').on('loginscreen:opened', function (e) {
@@ -202,5 +263,9 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
 
   function fnRegistro() {
     mainView.router.navigate('/registro/');
+  }
+
+  function fnRegComplejo() {
+    mainView.router.navigate('/registro-canchas/');
   }
 })
