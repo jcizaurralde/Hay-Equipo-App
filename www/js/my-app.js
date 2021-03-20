bd20c911d63184c15f6fa2dd0d2b529c5d56ec7d
jcizaurralde <jcizaurralde@gmail.com>
2021-03-20 18:22:55 -0300

    Terminando la agenda del lado del usuario administrador

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -25,6 +25,7 @@ var app = new Framework7({
       { path: '/perfil/', url: 'perfil.html', },
       { path: '/registro-equipo/', url: 'registro-equipo.html', },
       { path: '/ver-agenda-admin/', url: 'ver-agenda-admin.html', },
+      { path: '/ver-dia-admin/', url: 'ver-dia-admin.html', },
       
     
     ]
@@ -75,6 +76,11 @@ var horaInicio = "";
 var horaFinal = "";
 var notificationAgenda = "";
 var colDias = "";
+var diaSelecAd = "";
+var inicio = "";
+var fin = "";
+var notificationErrorDia = "";
+
 
 
 // Handle Cordova Device Ready Event
@@ -126,6 +132,13 @@ $$(document).on('deviceready', function() {
       title: 'Hay Equipo App',
       subtitle: 'Agenda de turnos creada exitosamente',
       text: 'Ir a: ver agenda, para gestionar',
+      closeButton: true,
+    });
+    notificationErrorDia = app.notification.create({
+      icon: '<i class="f7-icons">hand_thumbsdown</i>',
+      title: 'Hay Equipo App',
+      subtitle: 'Error',
+      text: 'El día seleccionado no existe en su agenda, por favor vuelva a seleccionar una fecha',
       closeButton: true,
     });
     
@@ -284,12 +297,12 @@ $$(document).on('page:init', '.page[data-name="agenda-admin"]', function (e) {
     fechaInicio = $$('#fechaDesde').val();
     fechaFinal = $$('#fechaHasta').val();
     var cantHoras = parseInt(horaFinal) - parseInt(horaInicio);
-    console.log("Cantidad de horas"+cantHoras);
+    console.log("Cantidad de horarios "+ (cantHoras));
     var horarios = "";
     horarios = horaInicio +"a";
     var sumatoria = parseInt(horaInicio);
     var horaFin = parseInt(horaFinal) + 1;
-    for (var i = 0; i<parseInt(cantHoras); i++){
+    for (var i = 0; i<parseInt(cantHoras+1); i++){
       sumatoria += 1;
       console.log("Sumatoria: " + sumatoria);
       horarios += sumatoria.toString() + "," + sumatoria.toString() + "a" ;
@@ -340,13 +353,13 @@ $$(document).on('page:init', '.page[data-name="agenda-admin"]', function (e) {
     }
 
     console.log("Fecha inicio: " + fechaInicio + "Fecha Fin: " + fechaFinal);
-    var inicio = new Date (fechaInicio);
-    var fin = new Date (fechaFinal);
+    inicio = new Date (fechaInicio);
+    fin = new Date (fechaFinal);
     var transcurso = fin.getTime() - inicio.getTime();
     console.log("Transcurso: "+ transcurso);
     var trDias= transcurso/1000/60/60/24;
     console.log("dias: "+trDias);
-    console.log("Cantidad de horas: "+ cantHoras);
+    console.log("Cantidad de horas de juego: "+ (cantHoras+1));
     var hoy = new Date(inicio.getTime() + 86400000);
     console.log("Hoy vale: " + hoy);
     for (var i=0; i<trDias; i++){
@@ -363,89 +376,74 @@ $$(document).on('page:init', '.page[data-name="agenda-admin"]', function (e) {
       }
       var fechaConcat = año+"-"+mes+"-"+dia;
       console.log("Fecha Concatenada: "+ fechaConcat);
-      switch (cantHoras) {
+      var idDias = emailLogin + fechaConcat;
+      console.log("idDias: "+ idDias);
+      switch ((cantHoras+1)) {
         case 1:
-          var datosAgenda = { Usuario: emailLogin, Turno1: horario1 + "Libre"}
+          var datosAgenda = { Usuario: emailLogin, Turno1: horario1 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 2:
-          var datosAgenda = { Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre" }
+          var datosAgenda = { Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre" }
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 3:
           var datosAgenda = {
-            Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre",
-            Turno3: horario3 + "Libre"}
+            Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre",
+            Turno3: horario3 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 3:
           var datosAgenda = {
-            Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre",
-            Turno3: horario3 + "Libre"}
+            Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre",
+            Turno3: horario3 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 4:
           var datosAgenda = {
-            Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre",
-            Turno3: horario3 + "Libre", Turno4: horario4 + "Libre"}
+            Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre",
+            Turno3: horario3 +"-"+"Libre", Turno4: horario4 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 5:
           var datosAgenda = {
-            Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre",
-            Turno3: horario3 + "Libre", Turno4: horario4 + "Libre", Turno5: horario5 + "Libre"}
+            Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre",
+            Turno3: horario3 +"-"+"Libre", Turno4: horario4 +"-"+"Libre", Turno5: horario5 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 6:
           var datosAgenda = {
-            Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre",
-            Turno3: horario3 + "Libre", Turno4: horario4 + "Libre", Turno5: horario5 + "Libre",
-            Turno6: horario6 + "Libre"}
+            Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre",
+            Turno3: horario3 +"-"+"Libre", Turno4: horario4 +"-"+"Libre", Turno5: horario5 +"-"+"Libre",
+            Turno6: horario6 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 7:
           var datosAgenda = {
-            Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre",
-            Turno3: horario3 + "Libre", Turno4: horario4 + "Libre", Turno5: horario5 + "Libre",
-            Turno6: horario6 + "Libre", Turno7: horario7 + "Libre"}
+            Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre",
+            Turno3: horario3 +"-"+"Libre", Turno4: horario4 +"-"+"Libre", Turno5: horario5 +"-"+"Libre",
+            Turno6: horario6 +"-"+"Libre", Turno7: horario7 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
         case 8:
           var datosAgenda = {
-            Usuario: emailLogin, Turno1: horario1 + "Libre", Turno2: horario2 + "Libre",
-            Turno3: horario3 + "Libre", Turno4: horario4 + "Libre", Turno5: horario5 + "Libre",
-            Turno6: horario6 + "Libre", Turno7: horario7 + "Libre", Turno8: horario8 + "Libre"}
+            Usuario: emailLogin, Turno1: horario1 +"-"+"Libre", Turno2: horario2 +"-"+"Libre",
+            Turno3: horario3 +"-"+"Libre", Turno4: horario4 +"-"+"Libre", Turno5: horario5 +"-"+"Libre",
+            Turno6: horario6 +"-"+"Libre", Turno7: horario7 +"-"+"Libre", Turno8: horario8 +"-"+"Libre"}
           console.log("Datos agenda: " + datosAgenda);
-          colDias.doc(fechaConcat).set(datosAgenda);
+          colDias.doc(idDias).set(datosAgenda);
           break;
       }
     }
 
-    calendario = app.calendar.create({
-      inputEl: '#demo-calendar-modal',
-      /*dateFormat: {weekday: 'long', month: 'long', day: '2-digit', year: 'numeric'},
-      closeOnSelect: true,*/
-      openIn: 'customModal',
-      header: true,
-      footer: true,
-      minDate: inicio,
-      maxDate: fin,
-
-        on: {
-          closed: function () {
-            /*$$('#horarios').empty()*/
-          console.log(calendar.getValue());
-          }
-        }
-    });
     notificationAgenda.open();
     mainView.router.navigate('/ver-agenda-admin/');
       
@@ -456,6 +454,24 @@ $$(document).on('page:init', '.page[data-name="agenda-admin"]', function (e) {
 
 $$(document).on('page:init', '.page[data-name="ver-agenda-admin"]', function (e) {
   console.log(e);
+
+  calendario = app.calendar.create({
+    inputEl: '#demo-calendar-modal',
+    openIn: 'customModal',
+    header: true,
+    footer: true,
+    minDate: inicio,
+    maxDate: fin,
+
+    on: {
+      closed: function () {
+        diaSelecAd = calendario.getValue();
+        console.log("Dia seleccionado: " + diaSelecAd);
+        fnMuestraDiaSeleccionado();
+      }
+    }
+  });
+
   $$('#btnVerAgenda').on('click', fnAbreCalendar);
   $$('#btnCrearAgenda').on('click', fnCreaAgenda);
   $$('#btnVEVolver').on('click', function() {
@@ -469,11 +485,220 @@ $$(document).on('page:init', '.page[data-name="ver-agenda-admin"]', function (e)
   function fnAbreCalendar() {
     // VER PARA ABRIR EL CALENDARIO Y QUE QUEDE EL VALOR GUARDADO PARA LUEGO PODER VER ESE DIA EN PARTICULAR
     //LOS HORARIOS DE ESE DIA, CON EL VALOR "LIBRE" U "OCUPADO" (SI UN USUARIO TOMO EL TURNO)
+    calendario.open();
   }
+
+  function fnMuestraDiaSeleccionado() {
+    mainView.router.navigate('/ver-dia-admin/');
+  }
+
+})
+
+$$(document).on('page:init', '.page[data-name="ver-dia-admin"]', function (e) {
+  console.log(e);
+  var accionTurnos = app.actions.create({
+    buttons: [
+      {
+        text: 'Disponible',
+        onClick: function () {
+          var disponibilidad = "Libre";
+          fnValorTurno(disponibilidad);
+          app.dialog.alert('El turno cambio a: "Libre"');
+        },
+        bold: true
+      },
+      {
+        text: 'No disponible',
+        onClick: function () {
+          var disponibilidad = "Ocupado";
+          fnValorTurno(disponibilidad);
+          app.dialog.alert('El turno cambio a: "Ocupado"');
+        },
+      },
+    ]
+  })
+  console.log("Se creo el panel de botones" + accionTurnos);
+ 
+  diaSelecAd = new Date (diaSelecAd);
+  var diaSelec = diaSelecAd.getDate().toString();
+  var mesSelec = (diaSelecAd.getMonth() + 1).toString();
+  var añoSelec = diaSelecAd.getFullYear().toString();
+  if (diaSelec > 0 && diaSelec < 10) {
+    diaSelec = '0' + diaSelec;
+  }
+  if (mesSelec > 0 && mesSelec < 10) {
+    mesSelec = '0' + mesSelec;
+  }
+  var buscaFecha = añoSelec + "-" + mesSelec + "-" + diaSelec;
+  console.log("Busca fecha: " + buscaFecha);
+  var tituloFecha = diaSelec + "-" + mesSelec + "-" + añoSelec;
+  $$('#tituloFecha').text('Fecha seleccionada: ' + tituloFecha);
+  var fechaRef = colDias.doc(emailLogin + buscaFecha);
+  console.log(fechaRef);
+  fechaRef.get().then((doc) => {
+    if (doc.exists) {
+      var turno1 = doc.data().Turno1;
+      console.log("Turno 1: " + turno1);
+      if (turno1 == undefined){
+        console.log("No existe este horario");
+      }else{
+        $$('#btnesTurnos1').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno1">' + turno1 + '</a>')
+      }
+      var turno2 = doc.data().Turno2;
+      console.log("Turno 2: " + turno2);
+      if (turno2 == undefined){
+        console.log("No existe este horario");
+      }else{
+        $$('#btnesTurnos1').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno2">' + turno2 + '</a>')
+      }
+      var turno3 = doc.data().Turno3;
+      console.log("Turno 3: " + turno3);
+      if (turno3 == undefined){
+        console.log("No existe este horario");
+      }else{
+        $$('#btnesTurnos2').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno3">' + turno3 + '</a>')
+      }
+      var turno4 = doc.data().Turno4;
+      console.log("Turno 4: " + turno4);
+      if (turno4 == undefined){
+        console.log("No existe este horario");
+      }else{
+        $$('#btnesTurnos2').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno4">' + turno4 + '</a>')
+      }
+      var turno5 = doc.data().Turno5;
+      console.log("Turno 5: " + turno5);
+      if (turno5 == undefined){
+        console.log("No existe este horario");
+      }else{
+        $$('#btnesTurnos3').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno5">' + turno5 + '</a>')
+      }
+      var turno6 = doc.data().Turno6;
+      console.log("Turno 6: " + turno6);
+      if (turno6 == undefined){
+        console.log("No existe este horario");
+      }else{
+        $$('#btnesTurnos3').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno6">' + turno6 + '</a>')
+      }
+      var turno7 = doc.data().Turno7;
+      console.log("Turno 7: " + turno7);
+      if (turno7 == undefined){
+        console.log("No existe este horario");
+      }else{
+        $$('#btnesTurnos4').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno7">' + turno7 + '</a>')
+      }
+      var turno8 = doc.data().Turno8;
+      console.log("Turno 8: " + turno8);
+      if (turno8 == undefined){
+        console.log("No existe este horario");
+      } else{
+        $$('#btnesTurnos4').append('<a href="#" class="col-50 button btnturno button-raised button-round" id="turno8">' + turno8 + '</a>')
+      }
+    } else {
+      console.log("No such document!");
+      notificationErrorDia.open();
+      mainView.router.navigate('/ver-agenda-admin/');
+    }
+  }).catch((error) => { 
+    console.log("Error getting document:", error);
+  });
+
+  $$('.btnturno').on('click', fnAbreBotones);
+
+  function fnAbreBotones() {
+    accionTurnos.open();
+    console.log("Abre el panel de botones");
+    var btnHorario = $$(this.id).text();
+    console.log("horario a cambiar: " + btnHorario);
+  }
+
+  function fnValorTurno(disponibilidad) {
+    var dispTurno = disponibilidad;
+    switch (btnHorario) {
+      case turno1:
+        fechaRef.update({ Turno1: horario1 + "-" + dispTurno})
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      case turno2:
+        fechaRef.update({ Turno2: horario2 + "-" + dispTurno })
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      case turno3:
+        fechaRef.update({ Turno3: horario3 + "-" + dispTurno })
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      case turno4:
+        fechaRef.update({ Turno4: horario4 + "-" + dispTurno })
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      case turno5:
+        fechaRef.update({ Turno5: horario5 + "-" + dispTurno })
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      case turno6:
+        fechaRef.update({ Turno6: horario6 + "-" + dispTurno })
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      case turno7:
+        fechaRef.update({ Turno7: horario7 + "-" + dispTurno })
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      case turno8:
+        fechaRef.update({ Turno8: horario8 + "-" + dispTurno })
+          .then(function () {
+            console.log("actualizado ok");
+          })
+          .catch(function (error) {
+            console.log("Error: " + error);
+          });
+        break;
+      default:
+        console.log("Nada se modificó")
+        break;
+    }
+    
+  }
+
+  $$('#btnVolverDia').on('click', function () {
+    mainView.router.navigate('/ver-agenda-admin/');
+  })
 })
 
 $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
-  // Do something here when page with data-name="about" attribute loaded and initialized
   console.log(e);
   $$('#btnRegRegistrar').on('click', fnRegistrar);
   $$('#btnRegVolver').on('click', function fnVuelveIndex() {
@@ -514,7 +739,6 @@ $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
 })
 
 $$(document).on('page:init', '.page[data-name="registro-canchas"]', function (e) {
-  // Do something here when page with data-name="about" attribute loaded and initialized
   console.log(e);
   $$('#btnRegComp').on('click',fnRegAdmnistrador);
   $$('#btnVolverComp').on('click', function fnVuelveIndexComp() {
